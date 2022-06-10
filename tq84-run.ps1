4a4f37c5495a64a8f1a645ba9f04820b677db966
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-06-10 09:59:42 +0200

    n/a

--- tq84-run.ps1
+++ tq84-run.ps1
@@ -1,5 +1,5 @@
 #msbuild C:\Users\Rene\github\sandbox-attacksurface-analysis-tools\EditSection\EditSection.csproj
-#msbuild C:\Users\Rene\github\sandbox-attacksurface-analysis-tools\NtApiDotNet\NtApiDotNet.csproj
+msbuild C:\Users\Rene\github\sandbox-attacksurface-analysis-tools\NtApiDotNet\NtApiDotNet.csproj
 msbuild C:\Users\Rene\github\sandbox-attacksurface-analysis-tools\NtObjectManager\NtObjectManager.csproj
 # msbuild C:\Users\Rene\github\sandbox-attacksurface-analysis-tools\NtApiDotNet\NtApiDotNet.Build.csproj
 
@@ -7,5 +7,5 @@ cp C:\Users\Rene\github\sandbox-attacksurface-analysis-tools\bin\Debug\NtObjectM
 
 add-type -path "$pwd\bin\debug\NtApiDotNet.dll"
 
-cd NtObjectManager\
-import-module NtObjectManager.psd1
+# cd NtObjectManager\
+Import-Module "$pwd\NtObjectManager\NtObjectManager.psd1"
