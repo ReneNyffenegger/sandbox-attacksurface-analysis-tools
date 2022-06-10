e19960b8d045a990a00e080b664a08ca8a0f5786
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-06-10 11:14:44 +0200

    n/a

--- NtApiDotNet/tq84.cs
+++ NtApiDotNet/tq84.cs
@@ -3,10 +3,26 @@ public class tq84 {
 
    private static int indent_ = 0;
 
-   public static void indent(string txt) { indent_ ++ ; print(txt);}
+   public static void indent(string txt,
+     [System.Runtime.CompilerServices.CallerMemberName        ] string mbr = "",
+     [System.Runtime.CompilerServices.CallerFilePath          ] string fil = "",
+     [System.Runtime.CompilerServices.CallerLineNumber        ] int    lin =  0
+//   [System.Runtime.CompilerServices.CallerArgumentExpression] string arg = "") // Implemented in compiler for C# 10 and later
+   ) {
+        indent_ ++;
+        print(txt, mbr, fil, lin);
+   }
+
    public static void dedent() { indent_ -- ;}
-   public static void print(string txt) {
-      System.Console.WriteLine(System.String.Empty.PadLeft(indent_*2) + txt);
+   public static void print(string txt,
+     [System.Runtime.CompilerServices.CallerMemberName        ] string mbr = "",
+     [System.Runtime.CompilerServices.CallerFilePath          ] string fil = "",
+     [System.Runtime.CompilerServices.CallerLineNumber        ] int    lin =  0
+//   [System.Runtime.CompilerServices.CallerArgumentExpression] string arg = "") // Implemented in compiler for C# 10 and later
+
+   ) {
+
+      System.Console.WriteLine(System.String.Empty.PadLeft(indent_*2) + txt + " (" + fil + "@" + lin + ", " + mbr + ")");
    }
 
 }
